1de2961da6aca5e5847245485995af97edbc6308
Sastri <141601910+sbhagwandass@users.noreply.github.com>
2025-08-05 20:32:11 -0400

    Create GreatestCommon DivisorOfStrings.cpp

---
+++ algorithms/cpp/GreatestCommon DivisorOfStrings.cpp
@@ -0,0 +1,52 @@
+/************************************************************************************************************************************************
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+************************************************************************************************************************************************/
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) 
+    {
+        if(str1.length() < str2.length())
+        {
+            return gcdOfStrings(str2,str1);
+        }
+
+        else if(str1.find(str2) != 0)
+        {
+            return "";
+        }
+
+        else if (str2 == "")
+        {
+            return str1;
+        }
+
+        else
+        {
+            return gcdOfStrings(str1.substr(str2.length()),str2);
+        }
+    }
+};
